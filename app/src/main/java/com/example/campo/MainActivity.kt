3441bf921e07408f01e37d8e6f9ed2a57cefacd5
Jorge <jorge.gutierrez4202@alumnos.udg.mx>
2024-03-27 10:51:06 -0600

    campo

--- app/src/main/java/com/example/campo/MainActivity.kt
+++ app/src/main/java/com/example/campo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.campo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+    lateinit var spCultivos : Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        spCultivos = findViewById(R.id.spCultivos)
+
     }
 }
