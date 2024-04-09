b08a7e2b24e2aa9e14978c0108dab4603b9561f8
Jorge <jorge.gutierrez4202@alumnos.udg.mx>
2024-04-09 11:41:24 -0600

    campo

--- app/src/main/java/com/example/campo/MainActivity.kt
+++ app/src/main/java/com/example/campo/MainActivity.kt
@@ -49,11 +49,15 @@ class MainActivity : AppCompatActivity() {
         spinner.setAdapter(adapterSp)
 
         btGuardar.setOnClickListener {
-
+            var maiz = "Maíz"
             var tipo = spinner.getSelectedItem().toString();
             var name = etNombre.text.toString()
             vmculti.elementos.add(tarea("$name","$tipo", false))
             adapter.notifyDataSetChanged()
+            if (tipo = maiz)
+            {
+
+            }
         }
 
     }
