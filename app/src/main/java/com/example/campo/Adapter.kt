40242f83f3ede2cac83f5b1465565adb8358bdbe
Jorge_LAB <neswar12@hotmail.com>
2024-05-10 10:47:02 -0600

    LAB

--- app/src/main/java/com/example/campo/Adapter.kt
+++ app/src/main/java/com/example/campo/Adapter.kt
@@ -3,14 +3,19 @@ package com.example.campo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+
+
+
 
 class Adapter(private val elementos: MutableList<tarea>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvCultivo: TextView = view.findViewById(R.id.tvCultivo)
         val tvTipo: TextView = view.findViewById(R.id.tvTipo)
+        val btnEliminar = itemView.findViewById<Button>(R.id.btnEliminar)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,7 +27,14 @@ class Adapter(private val elementos: MutableList<tarea>) : RecyclerView.Adapter<
         val item = elementos[position]
         holder.tvCultivo.text = item.Nombre
         holder.tvTipo.text = item.Tipo
-    }
 
-    override fun getItemCount(): Int = elementos.size
-}
+        holder.btnEliminar.setOnClickListener {
+            elementos.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, elementos.size)
+        }
+    }
+        override fun getItemCount(): Int = elementos.size
+
+
+    }
--- app/src/main/java/com/example/campo/viewholder.kt
+++ app/src/main/java/com/example/campo/viewholder.kt
@@ -1,10 +1,12 @@
 package com.example.campo
 
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class viewholder(itemView:View) : RecyclerView.ViewHolder(itemView) {
     val tvCultivo = itemView.findViewById<TextView>(R.id.tvCultivo)
     val tvTipo = itemView.findViewById<TextView>(R.id.tvTipo)
+    val btnEliminar = itemView.findViewById<Button>(R.id.btnEliminar)
 }
